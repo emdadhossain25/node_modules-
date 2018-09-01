9dcc45dbe0a7fffd54b5c4d7f66fb8e201e4e70d
emdad <emdad.hossain321@gmail.com>
2018-09-01 22:21:24 +0600

    added form data adding to route user_create

--- app.js
+++ app.js
@@ -3,20 +3,46 @@ var app = express();
 
 const mysql = require('mysql');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-// app.use(morgan('combined'));
 
 
-app.get('/users/:id',(req,res)=>{
-  console.log('we are fetching data for id: '+ req.params.id);
-  const connection = mysql.createConnection({
+app.use(morgan('short'));
+app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({extended:false}));
+
+function getConnection(){
+  return mysql.createConnection({
     host: '192.168.56.1',
     user:'emdad',
     password:'emdad123123',
     database: 'lbta_mysql'
-
   })
+}
 
+app.post('/user_create',(req,res)=>{
+  console.log('trying to create a new user ...')
+  console.log(req.body.create_first_name)
+  console.log(req.body.create_last_name)
+
+  const firstName = req.body.create_first_name;
+  const lastName = req.body.create_last_name;
+  
+  const queryString = 'INSERT INTO users(first_name,last_name) VALUES(?,?)';
+  getConnection().query(queryString,[firstName,lastName],(error,rows,fields)=>{
+    if(error){
+      console.log(error)
+      res.sendStatus(500);
+    }
+    console.log("data "+rows.insertId);
+    res.end()
+  })
+  
+})
+
+app.get('/users/:id',(req,res)=>{
+  console.log('we are fetching data for id: '+ req.params.id);
+  const connection = getConnection();
   const userId = req.params.id;
   const queryString ="SELECT * FROM  users WHERE id =?" 
   connection.query(queryString,[userId],(error,rows,fields)=>{
@@ -39,12 +65,38 @@ app.get('/users/:id',(req,res)=>{
   // res.end();
 })
 
-app.get("/users", (req, res) =>{
-var user1 = {firstName:"Stephen",lastName:"Curry"};
-const user2 = {firstName:"Emdad",lastName:"Hossain"};
-// res.send('nodemon auto updates when i save this file');
-  res.json([user1,user2]);
 
+
+  /**
+   * getting all users 
+   */
+
+app.get('/users',(req,res)=>{
+  // console.log('we are fetching data for id: '+ req.params.id);
+  const connection = getConnection();
+
+
+  
+
+  const queryString ="SELECT * FROM  users" 
+  connection.query(queryString,(error,rows,fields)=>{
+
+    if(error){
+      console.log('I think we failed got an error: '+error);
+      res.sendStatus(500)
+      return;
+    }
+
+    const users = rows.map((row) => {
+      return{firstName: row.first_name, lastName: row.last_name};
+    })
+    res.json(users);
+    // console.log("I think we successfully fetched")
+   
+  });
+
+
+  // res.end();
 })
 
 app.get("/", (req, res) =>{
